ad1897ffb453fa17cb896493ce425faf82235fd1
UpadhyayPuru <100061745+UpadhyayPuru@users.noreply.github.com>
2022-05-07 17:38:25 -0400

    Add files via upload

---
+++ QuinticPolynomial.py
@@ -0,0 +1,79 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class QuinticPolynomial:
+    def __init__(self,init_pos,init_vel,init_acc,final_pos,final_vel,final_acc,T):
+
+        # s(t) = a0 + a1*t + a2*t^2 + a3*t^3 + a4*t^4 + a5*t^5
+
+        self.a0 = init_pos
+        self.a1 = init_vel
+        self.a2 = init_acc
+
+        A = [
+            [T**3,T**4,T**5],
+            [3*T**2,4*T**3,5*T**4],
+            [6*T,12*T**2,20*T**3]
+            ]
+
+        B = [
+            [final_pos-self.a0-self.a1*T-self.a2*T**2],
+            [final_vel-self.a1-2*self.a2*T],
+            [final_acc-2*self.a2]
+            ]
+
+        x = np.linalg.solve(A,B)
+
+        self.a3 = x[0]
+        self.a4 = x[1]
+        self.a5 = x[2]
+
+
+    def cal_position(self,t):
+
+        xt = self.a0 + self.a1*t + self.a2*(t**2) + self.a3*(t**3) + self.a4*(t**4) + self.a5*(t**5)
+
+        return xt
+
+    def cal_velocity(self,t):
+
+        vt = self.a1 + 2*self.a2*t + 3*self.a3*(t**2) + 4*self.a4*(t**3) + 5*self.a5*(t**4)
+
+        return vt
+
+    def cal_acceleration(self,t):
+
+        at = 2*self.a2 + 6*self.a3*t + 12*self.a4*(t**2) + 20*self.a5*(t**3)
+
+        return at
+
+    def cal_jerk(self,t):
+
+        jt = 6*self.a3 + 24*self.a4*t + 60*self.a5*(t**2)
+
+        return jt
+
+def main():
+    x0 = 0
+    v0 = 0
+    a0 = 0
+    x1 = 5
+    v1 = 4
+    a1 = 0
+    T = 5
+    traj = QuinticPolynomial(x0,v0,a0,x1,v1,a1,T)
+    pos = []
+    time = []
+    dt = 0
+    while dt<5:
+        pos.append(traj.cal_position(dt))
+        time.append(dt)
+        dt = dt + 0.1
+
+    plt.plot(time,pos)
+    plt.show()
+
+
+
+if __name__=="__main__":
+    main()
